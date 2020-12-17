af55bb76f0f28089bd9b7a111da4c5931be759b4
ShwethaAayush <ShwethaAayush@github.com>
2020-12-17 23:39:57 +0530

    first commit

---
+++ springboot-docker-k8s-mysql/src/main/java/com/shwetha/training/SpringbootDockerK8sMysqlApplication.java
@@ -0,0 +1,13 @@
+package com.shwetha.training;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDockerK8sMysqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootDockerK8sMysqlApplication.class, args);
+	}
+
+}
